3071a76be7eab8327cad440935cda8695fd3ed52
AntoineLudwigGalli <antoine@ludwig.com>
2022-07-08 10:45:21 +0200

    ajout section podcast et carousel news sur l'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/", name: "main_")]
 class MainController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function home(): Response
     {
+        $bgc = "blue";
+
         return $this->render('main/home.html.twig', [
             'controller_name' => 'MainController',
+            'bgc' => $bgc,
+        ]);
+    }
+
+    #[Route('/news', name: 'news')]
+    public function news(): Response
+    {
+        $bgc = "red";
+
+        return $this->render('main/news.html.twig', [
+            'controller_name' => 'MainController',
+            'bgc' => $bgc,
         ]);
     }
 }
