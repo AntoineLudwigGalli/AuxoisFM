adff3bddc4a72e0ea8070d2432962544e1b26d56
AntoineLudwigGalli <antoine@ludwig.com>
2022-07-18 13:37:35 +0200

    fix user bar

--- src/Entity/Podcast.php
+++ src/Entity/Podcast.php
@@ -14,9 +14,6 @@ class Podcast
     #[ORM\Column()]
     private ?int $id = null;
 
-    #[ORM\Column(length: 300)]
-    private ?string $name = null;
-
     #[ORM\Column(length: 100)]
     private ?string $podcastLink = null;
 
@@ -33,18 +30,6 @@ class Podcast
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getPodcastLink(): ?string
